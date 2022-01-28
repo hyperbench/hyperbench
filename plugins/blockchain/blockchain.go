fb42bf1fa8813819900c148bec32e7971735a29b
linguopeng <linguopeng@hyperchain.cn>
2022-01-28 14:26:26 +0800

    feat(upgrade hyperchain-commin)

--- plugins/blockchain/blockchain.go
+++ plugins/blockchain/blockchain.go
@@ -28,7 +28,7 @@ func InitPlugin() {
 }
 
 // NewBlockchain create blockchain with different client type.
-func NewBlockchain(clientConfig base.ClientConfig) (client Blockchain, err error) {
+func NewBlockchain(clientConfig base.ClientConfig) (client fcom.Blockchain, err error) {
 	clientBase := base.NewBlockchainBase(clientConfig)
 	newFunc, err := plugins.Lookup("New")
 	if err != nil {
@@ -39,52 +39,9 @@ func NewBlockchain(clientConfig base.ClientConfig) (client Blockchain, err error
 	if err != nil {
 		return nil, err
 	}
-	client, ok := Client.(Blockchain)
+	client, ok := Client.(fcom.Blockchain)
 	if !ok {
 		return nil, errors.New(fmt.Sprint(reflect.TypeOf(client)) + " is not blockchain.Blockchain")
 	}
 	return
-}
-
-// Blockchain define the service need provided in blockchain.
-type Blockchain interface {
-
-	// DeployContract should deploy contract with config file
-	DeployContract() error
-
-	// Invoke just invoke the contract
-	Invoke(fcom.Invoke, ...fcom.Option) *fcom.Result
-
-	// Transfer a amount of money from a account to the other one
-	Transfer(fcom.Transfer, ...fcom.Option) *fcom.Result
-
-	// Confirm check the result of `Invoke` or `Transfer`
-	Confirm(*fcom.Result, ...fcom.Option) *fcom.Result
-
-	// Query do some query
-	Query(fcom.Query, ...fcom.Option) interface{}
-
-	// Option pass the options to affect the action of client
-	Option(fcom.Option) error
-
-	// GetContext Generate TxContext based on New/Init/DeployContract
-	// GetContext will only be run in master
-	// return the information how to invoke the contract, maybe include
-	// contract address, abi or so.
-	// the return value will be send to worker to tell them how to invoke the contract
-	GetContext() (string, error)
-
-	// SetContext set test context into go client
-	// SetContext will be run once per worker
-	SetContext(ctx string) error
-
-	// ResetContext reset test group context in go client
-	ResetContext() error
-
-	// Statistic query the statistics information in the time interval defined by
-	// nanosecond-level timestamps `from` and `to`
-	Statistic(statistic fcom.Statistic) (*fcom.RemoteStatistic, error)
-
-	// LogStatus records blockheight and time
-	LogStatus() (int64, error)
 }
